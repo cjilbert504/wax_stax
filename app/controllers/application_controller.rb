258f9ad2d534d04eb3cb6564dcac591b9bb5e9c0
Collin Jilbert <cjilbert504@gmail.com>
2020-03-02 09:26:39 -0600

    added helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def is_logged_in?
+      !!session[user_id]
+    end
+
+    def current_user
+      User.find(session[:user_id])
+    end
+
+  end
+
 end
