7c4203e3f5b86d3892436ea8712d86922e3bdec6
Collin Jilbert <cjilbert504@gmail.com>
2020-03-02 10:26:39 -0600

    built out user signup process

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
     erb :"/users/new.html"
   end
 
- 
   post "/signup" do
-    
+    @user = User.create(:username => params[:username], :password => params[:password])
+    session[:user_id] = @user.id
+    redirect "/users/#{@user.id}"
   end
 
-  # GET: /users/5
   get "/users/:id" do
+    @user = User.find(params[:id])
     erb :"/users/show.html"
   end
 
