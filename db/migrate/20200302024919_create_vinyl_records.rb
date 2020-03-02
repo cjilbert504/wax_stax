40c36947adc44b48048bfd2d513368b4c9ce32ea
Collin Jilbert <cjilbert504@gmail.com>
2020-03-02 08:37:27 -0600

    added column names to migrations

--- db/migrate/20200302024919_create_vinyl_records.rb
+++ db/migrate/20200302024919_create_vinyl_records.rb
@@ -1,7 +1,10 @@
 class CreateVinylRecords < ActiveRecord::Migration
   def change
     create_table :vinyl_records do |t|
-
+      t.string :artist
+      t.string :album
+      t.string :genre
+      t.string :release_date
       t.timestamps null: false
     end
   end
