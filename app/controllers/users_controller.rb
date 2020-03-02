95d50d8605bf50dc52a46410e38c5b1fec1bb7ca
Collin Jilbert <cjilbert504@gmail.com>
2020-03-02 11:54:04 -0600

    added user log out route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,12 +24,19 @@ class UsersController < ApplicationController
 
   post "/login" do
     user = User.find_by(:username => params[:username])
-    binding.pry 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect to "/users/#{user.id}"
     end
     redirect to "/login"
+  end
+
+  post "/logout" do
+    if session[:user_id] != nil
+      session.clear
+      redirect to "/login"
+    end
+    redirect to "/"
   end
 
   get "/users/:id" do
