4980b4b6efd5cb85f376d289d6ac1ec610e5dabd
Collin Jilbert <cjilbert504@gmail.com>
2020-03-03 16:01:20 -0600

    built out post create route

--- app/controllers/vinyl_records_controller.rb
+++ app/controllers/vinyl_records_controller.rb
@@ -1,20 +1,29 @@
 class VinylRecordsController < ApplicationController
 
   # GET: /vinyl_records
-  get "/vinyl_records" do
-    erb :"/vinyl_records/index.html"
-  end
+  # get "/vinyl_records" do
+  #   erb :"/vinyl_records/index.html"
+  # end
 
   get "/vinyl_records/new" do
     erb :"/vinyl_records/new.html"
   end
 
   post "/vinyl_records" do
-    redirect "/vinyl_records"
+    if !is_logged_in?
+      redirect to "/"
+    end
+    if params[:artist].empty? || params[:album].empty? || params[:genre].empty? || params[:release_date].empty?
+      redirect to "/vinyl_records/new"
+    else
+      params[:user_id] = current_user.id
+      vinyl_record = VinylRecord.create(params)
+      redirect "/vinyl_records/#{vinyl_record.id}"
+    end
   end
 
-  # GET: /vinyl_records/5
   get "/vinyl_records/:id" do
+
     erb :"/vinyl_records/show.html"
   end
 
