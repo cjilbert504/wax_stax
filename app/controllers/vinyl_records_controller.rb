53914367028de99674831ebb26c53939dc9f6a0c
Collin Jilbert <cjilbert504@gmail.com>
2020-03-03 22:07:47 -0600

    built out record edit routes

--- app/controllers/vinyl_records_controller.rb
+++ app/controllers/vinyl_records_controller.rb
@@ -1,10 +1,5 @@
 class VinylRecordsController < ApplicationController
 
-  # GET: /vinyl_records
-  # get "/vinyl_records" do
-  #   erb :"/vinyl_records/index.html"
-  # end
-
   get "/vinyl_records/new" do
     erb :"/vinyl_records/new.html"
   end
@@ -27,14 +22,15 @@ class VinylRecordsController < ApplicationController
     erb :"/vinyl_records/show.html"
   end
 
-  # GET: /vinyl_records/5/edit
   get "/vinyl_records/:id/edit" do
+    @vinyl_record = VinylRecord.find(params[:id])
     erb :"/vinyl_records/edit.html"
   end
 
-  # PATCH: /vinyl_records/5
   patch "/vinyl_records/:id" do
-    redirect "/vinyl_records/:id"
+    @vinyl_record = VinylRecord.find(params[:id])
+    @vinyl_record.update(params[:record])
+    redirect "/vinyl_records/#{@vinyl_record.id}"
   end
 
   # DELETE: /vinyl_records/5/delete
